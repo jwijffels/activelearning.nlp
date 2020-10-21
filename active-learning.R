1c63b9231323ea2c1d54b5e85342747b5809fb0a
Jan Wijffels <jwijffels@bnosac.be>
2020-10-21 21:42:05 +0200

    make sure pattern is a list - allowing to move to a multi-class classification

--- active-learning.R
+++ active-learning.R
@@ -40,37 +40,45 @@ activelearning <- function(X, Y, data = X,
     ##
     ## Similarity based on regular expression
     ##
-    stopifnot(is.character(pattern) && length(pattern) == 1)
+    stopifnot(is.list(pattern) && all(sapply(pattern, is.character)) && all(sapply(pattern, length) == 1))
     data_transformed <- FUN(data)
     ## TIF data.frame with fields doc_id and text
     stopifnot(is_tif(data_transformed))
     stopifnot(nrow(data_transformed) == length(Y))
     data_transformed <- data_transformed[idx$unknown, , drop = FALSE]
-    similarity <- grepl(x = data_transformed$text, pattern = pattern, ...)
-    similarity <- as.numeric(similarity)
-    out   <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity)
+    similarity <- lapply(pattern, FUN=function(pattern, x, ...){
+      similarity <- grepl(x = x, pattern = pattern, ...)
+      similarity <- as.numeric(similarity)  
+      similarity
+    }, x = data_transformed$text, ...)
+    out   <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity)
   }else if(type == "stringdist"){
     ##
     ## Similarity based on maximum string similarity
     ##
-    stopifnot(is.character(pattern) && length(pattern) == 1)
+    stopifnot(is.list(pattern) && all(sapply(pattern, is.character)) && all(sapply(pattern, length) == 1))
     data_transformed <- FUN(data)
     if(is.data.frame(data_transformed)){
       ## TIF data.frame with fields doc_id and text
       stopifnot(is_tif(data_transformed))
       stopifnot(nrow(data_transformed) == length(Y))
       data_transformed <- data_transformed[idx$unknown, , drop = FALSE]
-      similarity <- stringdist::stringsim(a = data_transformed$text, b = pattern, ...)  
+      similarity <- lapply(pattern, FUN=function(pattern, x, ...){
+        similarity <- stringdist::stringsim(a = x, b = pattern, ...)  
+        similarity
+      }, x = data_transformed$text, ...)
     }else{
       ## list of words: take the highest similarity
       data_transformed <- data_transformed[idx$unknown]
-      similarity <- sapply(data_transformed, FUN=function(x, pattern, ...){
-        similarity <- stringdist::stringsim(a = x, b = pattern, ...)    
-        similarity <- AGG(similarity)
-        similarity
-      }, pattern = pattern, ...)
+      similarity <- lapply(pattern, FUN=function(pattern, x, ...){
+        similarity <- sapply(x, FUN=function(x, pattern, ...){
+          similarity <- stringdist::stringsim(a = x, b = pattern, ...)    
+          similarity <- AGG(similarity)
+          similarity
+        }, pattern = pattern, ...)
+      }, x = data_transformed, ...)
     }
-    out   <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity)
+    out   <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity)
   }else if(type == "naive_bayes"){
     #model <- fastNaiveBayes::fastNaiveBayes(X, Y, ...)
     .NotYetImplemented()
@@ -86,7 +94,7 @@ activelearning <- function(X, Y, data = X,
     newdata     <- X[idx$unknown, , drop = FALSE]
     similarity  <- predict(model, newdata, s = "lambda.1se", type = "response")
     similarity  <- drop(similarity)
-    out         <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity, model = model)
+    out         <- list(type = type, pattern = list(), args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity, model = model)
     # label_order <- apply(scores, MARGIN = 1, FUN = entropy::entropy.plugin)
     # label_order <- data.frame(doc_id = names(label_order),
     #                           entropy = round(as.numeric(label_order), 5),
@@ -100,56 +108,72 @@ activelearning <- function(X, Y, data = X,
   }else if(type == "nametagger"){
     .NotYetImplemented()
   }else if(type == "svd_similarity"){
+    stopifnot(is.list(pattern) && all(sapply(pattern, FUN=function(x) all(is.numeric(x)))))
     ##
     ## Similarity based on singular value decomposition
     ##
-    data_transformed <- FUN(data)
+    if(missing(X)){
+      X <- FUN(data)
+    }
     ## should be a sparse matrix
     dtm_svd <- function(dtm, dim = 5, ...){
       SVD <- RSpectra::svds(dtm, nu = 0, k = dim, ...)
       rownames(SVD$v) <- colnames(dtm)
       SVD$v
     }
-    embedding  <- dtm_svd(data_transformed, dim = ldots$dim)
-    similarity <- udpipe::dtm_svd_similarity(data_transformed, embedding = embedding, weights = pattern)
-    similarity <- similarity$similarity$similarity[idx$unknown]
-    out        <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity, model = embedding)
+    embedding  <- dtm_svd(X, dim = ldots$dim)
+    similarity <- lapply(pattern, FUN=function(pattern){
+      similarity <- udpipe::dtm_svd_similarity(X, embedding = embedding, weights = pattern)
+      similarity <- similarity$similarity$similarity[idx$unknown]
+      similarity
+    })
+    out        <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity, model = embedding)
   }else if(type == "word2vec"){
+    stopifnot(is.list(pattern) && all(sapply(pattern, is.character)) && all(sapply(pattern, length) == 1))
     ##
     ## Similarity based on doc2vec to the pattern string
     ##
-    data_transformed <- FUN(data)
+    if(missing(X)){
+      X <- FUN(data)
+    }
     ## list of words, get embedding of document and pattern and find similarity
-    stopifnot(is.list(data_transformed))
-    stopifnot(is.character(pattern) && length(pattern) == 1)
-    pattern_vector   <- word2vec::doc2vec(object, newdata = pattern) ## works also on multi-word expressions separated by space
-    data_transformed <- data_transformed[idx$unknown]
-    similarity       <- sapply(data_transformed, FUN=function(x, pattern_vector){
-      wordvectors <- predict(object, newdata = x, type = "embedding")
-      similarity  <- word2vec::word2vec_similarity(wordvectors, pattern_vector)
-      similarity  <- drop(similarity)
-      similarity  <- similarity[!is.na(similarity)]
-      if(length(similarity) == 0){
-        return(NA_real_)
-      }else{
-        max(similarity)
-      }
-    }, pattern_vector = pattern_vector)
-    out              <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity, model = object)
+    stopifnot(is.list(X))
+    X                <- X[idx$unknown]
+    similarity <- lapply(pattern, FUN = function(pattern){
+      pattern_vector   <- word2vec::doc2vec(object, newdata = pattern) ## works also on multi-word expressions separated by space
+      similarity       <- sapply(X, FUN=function(x, pattern_vector){
+        wordvectors <- predict(object, newdata = x, type = "embedding")
+        similarity  <- word2vec::word2vec_similarity(wordvectors, pattern_vector)
+        similarity  <- drop(similarity)
+        similarity  <- similarity[!is.na(similarity)]
+        if(length(similarity) == 0){
+          return(NA_real_)
+        }else{
+          max(similarity)
+        }
+      }, pattern_vector = pattern_vector)
+      similarity
+    })
+    out              <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity, model = object)
   }else if(type == "doc2vec"){
+    stopifnot(is.list(pattern) && all(sapply(pattern, is.character)) && all(sapply(pattern, length) == 1))
     ##
     ## Similarity based on doc2vec to the pattern string
     ##
-    data_transformed <- FUN(data)
+    if(missing(X)){
+      X <- FUN(data)
+    }
     ## list of words, get embedding of document and pattern and find similarity
-    stopifnot(is.list(data_transformed))
-    stopifnot(is.character(pattern) && length(pattern) == 1)
-    pattern_vector   <- word2vec::doc2vec(object, newdata = pattern) ## works also on multi-word expressions separated by space
-    data_transformed <- data_transformed[idx$unknown]
-    doc_vectors      <- word2vec::doc2vec(object, data_transformed, ...)
-    similarity       <- word2vec::word2vec_similarity(doc_vectors, pattern_vector)
-    similarity       <- drop(similarity)
-    out              <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, similarity = similarity, model = object)
+    stopifnot(is.list(X))
+    X                <- X[idx$unknown]
+    doc_vectors      <- word2vec::doc2vec(object, X, ...)
+    similarity       <- lapply(pattern, FUN = function(pattern){
+      pattern_vector   <- word2vec::doc2vec(object, newdata = pattern) ## works also on multi-word expressions separated by space
+      similarity       <- word2vec::word2vec_similarity(doc_vectors, pattern_vector)
+      similarity       <- drop(similarity)
+      similarity
+    })
+    out              <- list(type = type, pattern = pattern, args = ldots, idx = idx, data = data, FUN = FUN, similarity = similarity, model = object)
   }
   class(out) <- "activelearning"
   out
@@ -161,12 +185,12 @@ sample <- function(x, ...){
     base::sample(x, ...)
   }
 }
-sample.activelearning <- function(x, type = c("random", "entropy", "bagging", "committee")){
+sample.activelearning <- function(x, which = head(names(x$similarity), 1), type = c("random", "entropy", "bagging", "committee")){
   type <- match.arg(type)
   ## TODO
   records <- x$data[x$idx$unknown, ]
-  records$similarity <- x$similarity
-  records <- records[order(records$similarity, decreasing = TRUE), , drop = FALSE]
+  records[, names(x$similarity)] <- x$similarity
+  records <- records[order(records[[which]], decreasing = TRUE), , drop = FALSE]
   records
 }
 
@@ -179,11 +203,11 @@ DB <- data.frame(doc_id = movie_review$id,
                  stringsAsFactors = FALSE)
 as_dtm <- function(docs){
   docs$token <- tolower(docs$text)
-  x <- strsplit.data.frame(docs, "token", "doc_id", split = "[[:space:][:punct:][:digit:]]+")
-  x <- document_term_frequencies(x)
-  x <- document_term_matrix(x)
-  x <- dtm_remove_lowfreq(x, minfreq = 10)
-  x <- dtm_conform(x, rows = docs$doc_id)
+  x <- udpipe::strsplit.data.frame(docs, "token", "doc_id", split = "[[:space:][:punct:][:digit:]]+")
+  x <- udpipe::document_term_frequencies(x)
+  x <- udpipe::document_term_matrix(x)
+  x <- udpipe::dtm_remove_lowfreq(x, minfreq = 10)
+  x <- udpipe::dtm_conform(x, rows = docs$doc_id)
   x
 }
 txt_clean_w2v <- function(x){
@@ -195,27 +219,29 @@ txt_clean_w2v <- function(x){
   text
 }
 text <- txt_clean_w2v(DB$text)
-w2v  <- word2vec::word2vec(x = text, dim = 10, iter = 20, type = "cbow", min_count = 500)
+w2v  <- word2vec::word2vec(x = text, dim = 10, iter = 20, type = "cbow", min_count = 20)
 summary(w2v)
-predict(w2v, c("music", "acting"), type = "nearest")
+predict(w2v, c("music", "acting", "horrible"), type = "nearest")
 
 
-model <- activelearning(data = DB, Y = DB$target, type = "grep", pattern = "bad", ignore.case = TRUE)
-model <- activelearning(data = DB, Y = DB$target, type = "stringdist", pattern = "bad", FUN = function(x){
+model <- activelearning(data = DB, Y = DB$target, type = "grep", pattern = list(bad = "bad"), ignore.case = TRUE, size = 100)
+model <- activelearning(data = DB, Y = DB$target, type = "stringdist", pattern = list(bad = "bad"), FUN = function(x){
   x <- setNames(tolower(x$text), x$doc_id)
   strsplit(x, split = " ")
 })
-model <- activelearning(data = DB, Y = DB$target, type = "glmnet", family = "binomial", pattern = "bad", 
-                        FUN = function(docs) as_dtm(docs))
+model <- activelearning(data = DB, Y = DB$target, type = "glmnet", family = "binomial", FUN = as_dtm)
+X <- as_dtm(DB)
+model <- activelearning(data = DB, X = X, Y = DB$target, type = "svd_similarity", dim = 20,
+                        pattern = list(goodbad = setNames(c(1, 1, -1, -1), c("good", "fantastic", "bad", "worse"))))
 model <- activelearning(data = DB, Y = DB$target, type = "svd_similarity", dim = 20,
-                        pattern = setNames(c(1, 1, -1, -1), c("good", "fantastic", "bad", "worse")), 
-                        FUN = function(docs) as_dtm(docs))
-model <- activelearning(data = DB, Y = DB$target, type = "word2vec", object = w2v, pattern = "horrible", 
+                        pattern = list(goodbad = setNames(c(1, 1, -1, -1), c("good", "fantastic", "bad", "worse"))), 
+                        FUN = as_dtm)
+model <- activelearning(data = DB, Y = DB$target, type = "word2vec", object = w2v, pattern = list(badmovie = "bad"), 
                         FUN = function(docs){
                           docs <- setNames(txt_clean_w2v(docs$text), docs$doc_id)
                           strsplit(docs, split = " +")
                         }, AGG = function(x) quantile(x, 0.9))
-model <- activelearning(data = DB, Y = DB$target, type = "doc2vec", object = w2v, pattern = "bad", 
+model <- activelearning(data = DB, Y = DB$target, type = "doc2vec", object = w2v, pattern = list(badmovie = "horrible terrible"), 
                         FUN = function(docs){
                           docs <- setNames(txt_clean_w2v(docs$text), docs$doc_id)
                           strsplit(docs, split = " +")
